9512d3943c0136e9af5d30ab0d7202a79e8cc462
Zaccary Price <wildtangomedia@gmail.com>
2013-11-17 18:44:21 +0200

    hero section about section some initial work on credits section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,38 @@
+function heroImageScaling() {
+	var mobile = 480,
+		imgRatio = 1920/1200,
+		screenRatio = window.innerWidth/window.innerHeight,
+		hero = $('#hero');
+
+	console.log(imgRatio > screenRatio);
+	if(window.innerWidth > mobile) {
+		if(screenRatio > imgRatio) {
+			hero.css({
+				'-webkit-background-size': '100% auto',
+				'background-size': '100% auto'
+			});
+		} else {
+			hero.css({
+				'-webkit-background-size': 'auto 100%',
+				'background-size': 'auto 100%'
+			});
+		}
+	}
+
+}
+
+
 $(function() {
+	var height = window.innerHeight,
+		hero = $('#hero');
+
+	hero.css({
+		height: height + 'px'
+	});
+
+	heroImageScaling();
+
 	FastClick.attach(document.body);
+
+
 });
